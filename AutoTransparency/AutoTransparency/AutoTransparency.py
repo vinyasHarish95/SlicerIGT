75967e3d5301a30b5a4c0cca7db5276bb64c45df
vinyasHarish95 <vinyas.harish95@gmail.com>
2017-03-09 17:16:06 -0500

    Split up testing code into methods in logic and testing
    Specific models are drawn in testing, but logic computes cone dimensions and draws the cone.

--- AutoTransparency/AutoTransparency/AutoTransparency.py
+++ AutoTransparency/AutoTransparency/AutoTransparency.py
@@ -4,6 +4,7 @@ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 import logging
 import math
+import collections
 import numpy as np
 
 #-------------------------------------------------------------------------------
@@ -107,9 +108,144 @@ class AutoTransparencyWidget(ScriptedLoadableModuleWidget):
 #-------------------------------------------------------------------------------
 
 class AutoTransparencyLogic(ScriptedLoadableModuleLogic):
-  def logicMethod(self):
-    pass
+  def computeCenterOfMass(self, modelPolyData):
+    centerFilter = vtk.vtkCenterOfMass()
+    centerFilter.SetInputData(modelPolyData)
+    centerFilter.SetUseScalarsAsWeights(False)
+    centerFilter.Update()
+    return centerFilter
 
+  def getCameraPosition(self):
+    cameraNode = slicer.util.getNode('vtkMRMLCameraNode1')
+    cameraPosition = [0.0,0.0,0.0]
+    cameraNode.GetPosition(cameraPosition)
+    return cameraPosition
+
+  def computeConeDimensions(self, inputModelPolyData, inputCenterOfMass, cameraPosition):
+    #Find dMax
+    dMax = 0
+    numPoints = inputModelPolyData.GetNumberOfPoints()
+    pointTupleArray = []
+    #Create array of all points that make up the model
+    for point in range(numPoints):
+      pointTupleArray.append(inputModelPolyData.GetPoint(point))
+
+    for point in pointTupleArray:
+      dist = math.sqrt(vtk.vtkMath.Distance2BetweenPoints(point, inputCenterOfMass))
+      if (dist > dMax):
+        dMax = dist
+
+    #Find the angle, alpha
+    l = math.sqrt(vtk.vtkMath.Distance2BetweenPoints(cameraPosition,inputCenterOfMass))
+    alpha = math.asin(dMax/l)
+
+    #Find the radius of the cone, based on triangle 2
+    height = l
+    radius = (math.tan(alpha))*(height)
+
+    #Return dimensions as a named tuple
+    ConeDimensions = collections.namedtuple('ConeDimensions', ['height', 'radius'])
+    coneDimensions = ConeDimensions(height, radius)
+    return coneDimensions
+
+  def createConeSource(self, coneDimensions, center, resolution):
+    coneSource = vtk.vtkConeSource()
+    coneSource.SetRadius(coneDimensions.radius)
+    coneSource.SetHeight(coneDimensions.height)
+    coneSource.SetCenter(center)
+    coneSource.SetResolution(resolution)
+    return coneSource
+
+  def drawCone(self, coneSource, coneDimensions, center):
+    coneModelNode = slicer.vtkMRMLModelNode()
+    slicer.mrmlScene.AddNode(coneModelNode)
+    coneModelNode.SetName('ConeModel')
+    coneModelNodeToUpdate = coneModelNode
+    coneSource.Update()
+    coneModelNodeToUpdate.SetAndObservePolyData(coneSource.GetOutput())
+
+    if coneModelNodeToUpdate.GetDisplayNode() is None:
+      displayNode = slicer.vtkMRMLModelDisplayNode()
+      slicer.mrmlScene.AddNode(displayNode)
+      displayNode.SetName('ConeModelDisplay')
+      coneModelNodeToUpdate.SetAndObserveDisplayNodeID(displayNode.GetID())
+
+    return coneModelNode
+
+  def getConeTip(self, coneModelNode):
+    coneModelPolyData = coneModelNode.GetPolyData()
+    numPointsInCone = coneModelPolyData.GetNumberOfPoints()
+    conePointsTupleArray = []
+    for point in range(numPointsInCone):
+      conePointsTupleArray.append(coneModelPolyData.GetPoint(point))
+    coneTip = conePointsTupleArray[0]
+
+    #Place fiducial at cone tip
+    markupsLogic = slicer.modules.markups.logic()
+    markupsLogic.SetDefaultMarkupsDisplayNodeGlyphScale(5.0)
+    markupsLogic.SetDefaultMarkupsDisplayNodeColor(0.0, 0.0, 0.0)
+    markupsLogic.SetDefaultMarkupsDisplayNodeSelectedColor(0.0, 0.0, 0.0)
+    markupsLogic.AddNewFiducialNode()
+    markupsLogic.AddFiducial(coneTip[0], coneTip[1], coneTip[2])
+    fidList = slicer.util.getNode('F')
+    fidList.SetNthFiducialLabel(0, 'Cone Tip')
+
+    return coneTip
+
+  def getCenterOfConeBase(self, coneSource, coneModelNode):
+    coneModelPolyData = coneModelNode.GetPolyData()
+    numPointsInCone = coneModelPolyData.GetNumberOfPoints()
+    conePointsTupleArray = []
+
+    for point in range(numPointsInCone):
+      conePointsTupleArray.append(coneModelPolyData.GetPoint(point))
+    coneBasePoints = []
+
+    for index in range(1, numPointsInCone):
+      point = conePointsTupleArray[index]
+      coneBasePoints.append(point)
+    xSum, ySum, zSum = 0, 0, 0
+    for (x,y,z) in coneBasePoints:
+      xSum += x
+      ySum += y
+      zSum += z
+    coneBaseCenter_x = xSum / coneSource.GetResolution()
+    coneBaseCenter_y = ySum / coneSource.GetResolution()
+    coneBaseCenter_z = zSum / coneSource.GetResolution()
+
+    #Place fiducial at cone base
+    markupsLogic = slicer.modules.markups.logic()
+    markupsLogic.SetDefaultMarkupsDisplayNodeGlyphScale(5.0)
+    markupsLogic.SetDefaultMarkupsDisplayNodeColor(0.0, 0.0, 0.0)
+    markupsLogic.SetDefaultMarkupsDisplayNodeSelectedColor(0.0, 0.0, 0.0)
+    markupsLogic.AddFiducial(coneBaseCenter_x, coneBaseCenter_y, coneBaseCenter_z)
+    fidList = slicer.util.getNode('F')
+    fidList.SetNthFiducialLabel(1, 'Cone Base Center')
+
+  def computeThirdPointsForLandmarkRegistration(self, cameraPosition, coneTip):
+    '''
+    Create a point perpendicular to conetip-axis
+    '''
+    pointCoplanarToConeTip = (coneTip[0], coneTip[1] + 100 , coneTip[2]) #Add arbitrary distance along plane
+    #markupsLogic.AddFiducial(pointCoplanarToConeTip[0],pointCoplanarToConeTip[1],pointCoplanarToConeTip[2])
+    midpointOfConeTipLine = ((coneTip[0] + pointCoplanarToConeTip[0])/2 , (coneTip[1] + pointCoplanarToConeTip[1])/2, (coneTip[2] + pointCoplanarToConeTip[2])/2)
+    #markupsLogic.AddFiducial(midpointOfConeTipLine[0],midpointOfConeTipLine[1],midpointOfConeTipLine[2])
+    perpendicularPointCone = (midpointOfConeTipLine[0], midpointOfConeTipLine[1], midpointOfConeTipLine[2] + 100)
+    #markupsLogic.AddFiducial(perpendicularPoint[0],perpendicularPoint[1],perpendicularPoint[2])
+    '''
+    Create a point perpendicular to camera-position axis, in the same way as above
+    '''
+    pointCoplanarToCameraPosition = (cameraPosition[0], cameraPosition[1] + 100 , cameraPosition[2]) #Add arbitrary distance along plane
+    #markupsLogic.AddFiducial(pointCoplanarToCameraPosition[0],pointCoplanarToCameraPosition[1],pointCoplanarToCameraPosition[2])
+    midpointOfCameraPositionLine = ((cameraPosition[0] + pointCoplanarToCameraPosition[0])/2 , (cameraPosition[1] + pointCoplanarToCameraPosition[1])/2, (cameraPosition[2] + pointCoplanarToCameraPosition[2])/2)
+    #markupsLogic.AddFiducial(midpointOfCameraPositionLine[0],midpointOfCameraPositionLine[1],midpointOfCameraPositionLine[2])
+    perpendicularPointRAS = (midpointOfCameraPositionLine[0], midpointOfCameraPositionLine[1], midpointOfCameraPositionLine[2] + 100)
+    #markupsLogic.AddFiducial(perpendicularPoint[0],perpendicularPoint[1],perpendicularPoint[2])
+
+    #Return third point pair as a named tuple
+    FiducialPairThree = collections.namedtuple('FiducialPairThree', ['perpendicularPointCone', 'perpendicularPointRAS'])
+    fidPairThree = FiducialPairThree(perpendicularPointCone, perpendicularPointRAS)
+    return fidPairThree
 
 #-------------------------------------------------------------------------------
 #
@@ -118,8 +254,6 @@ class AutoTransparencyLogic(ScriptedLoadableModuleLogic):
 #-------------------------------------------------------------------------------
 class AutoTransparencyTest(ScriptedLoadableModuleTest):
   def setUp(self):
-    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
-    """
     slicer.mrmlScene.Clear(0)
 
   def runTest(self):
@@ -128,16 +262,14 @@ class AutoTransparencyTest(ScriptedLoadableModuleTest):
     self.setUp()
     self.test_AutoTransparency()
 
-  def test_AutoTransparency(self):
-    self.delayDisplay("Starting the test")
-
+  def createSampleModels(self):
     #Create a cautery model
     moduleDirectoryPath = slicer.modules.autotransparency.path.replace('AutoTransparency.py', '')
     slicer.util.loadModel(qt.QDir.toNativeSeparators(moduleDirectoryPath + 'Resources/CAD/Cautery.stl'))
-    cauteryModelNode = slicer.util.getNode(pattern = "Cautery")
-    cauteryModelNode.GetDisplayNode().SetColor(1.0, 1.0, 0)
-    cauteryModelNode.SetName("CauteryModel")
-    cauteryModelNode.GetDisplayNode().SliceIntersectionVisibilityOn()
+    self.cauteryModelNode = slicer.util.getNode(pattern = "Cautery")
+    self.cauteryModelNode.GetDisplayNode().SetColor(1.0, 1.0, 0)
+    self.cauteryModelNode.SetName("CauteryModel")
+    self.cauteryModelNode.GetDisplayNode().SliceIntersectionVisibilityOn()
 
     #Create transform node and set transform of transform node
     cauteryModelToRas = slicer.vtkMRMLLinearTransformNode()
@@ -151,12 +283,12 @@ class AutoTransparencyTest(ScriptedLoadableModuleTest):
     cauteryModelToRas.SetAndObserveTransformToParent(cauteryModelToRasTransform)
 
     #Transform the needle model
-    cauteryModelNode.SetAndObserveTransformNodeID(cauteryModelToRas.GetID())
+    self.cauteryModelNode.SetAndObserveTransformNodeID(cauteryModelToRas.GetID())
 
     #Create a sphere tumor model
-    tumorModelNode = slicer.modules.createmodels.logic().CreateSphere(10)
-    tumorModelNode.GetDisplayNode().SetColor(0,1,0) #Green
-    tumorModelNode.SetName('TumorModel')
+    self.tumorModelNode = slicer.modules.createmodels.logic().CreateSphere(10)
+    self.tumorModelNode.GetDisplayNode().SetColor(0,1,0) #Green
+    self.tumorModelNode.SetName('TumorModel')
 
     #Create transform node and set transform of transform node
     tumorModelToRas = slicer.vtkMRMLLinearTransformNode()
@@ -166,15 +298,19 @@ class AutoTransparencyTest(ScriptedLoadableModuleTest):
     tumorModelToRas.SetAndObserveTransformToParent(tumorModelToRasTransform)
 
     #Transform the tumor model
-    tumorModelNode.SetAndObserveTransformNodeID(tumorModelToRas.GetID())
+    self.tumorModelNode.SetAndObserveTransformNodeID(tumorModelToRas.GetID())
+
+  def test_AutoTransparency(self):
+    self.delayDisplay("Starting the test")
+    #Create models of needle and cautery, in a position that does not represent
+    #collision
+    self.createSampleModels()
 
     #Compute the center of mass of the target model node
     tumorModel = slicer.mrmlScene.GetNodeByID('vtkMRMLModelNode5')
     tumorModelPolyData = tumorModel.GetPolyData()
-    centerFilter = vtk.vtkCenterOfMass()
-    centerFilter.SetInputData(tumorModelPolyData)
-    centerFilter.SetUseScalarsAsWeights(False)
-    centerFilter.Update()
+    testingLogic = AutoTransparencyLogic()
+    centerFilter = testingLogic.computeCenterOfMass(tumorModelPolyData)
 
     #Transform center to RAS coordinate system
     center = []
@@ -190,95 +326,29 @@ class AutoTransparencyTest(ScriptedLoadableModuleTest):
     print "Center of mass in RAS: ", center
 
     #Get the camera node's position
-    cam = slicer.util.getNode('vtkMRMLCameraNode1')
-    pos = [0.0,0.0,0.0]
-    cam.GetPosition(pos)
-    print "Camera position: ", pos
+    cameraPosition = testingLogic.getCameraPosition()
+    print "Camera position: ", cameraPosition
 
-    #Find dMax
-    dMax = 0
-    numPoints = tumorModelPolyData.GetNumberOfPoints()
-    pointTupleArray = []
-    #Create array of all points that make up the model
-    for point in range(numPoints):
-      pointTupleArray.append(tumorModelPolyData.GetPoint(point))
-
-    for point in pointTupleArray:
-      dist = math.sqrt(vtk.vtkMath.Distance2BetweenPoints(point,center))
-      if (dist > dMax):
-        dMax = dist
-
-    #Find the angle, alpha
-    l = math.sqrt(vtk.vtkMath.Distance2BetweenPoints(pos,center))
-    alpha = math.asin(dMax/l)
-
-    #Find the radius of the cone, based on triangle 2
-    height = l
-    radius = (math.tan(alpha))*(height)
+    #Find cone dimensions
+    coneDimensions = testingLogic.computeConeDimensions(tumorModelPolyData, center, cameraPosition)
 
     #Create a model of the cone and visualize it in the scene
-    cone = vtk.vtkConeSource()
-    cone.SetRadius(radius)
-    cone.SetHeight(height)
-    cone.SetCenter(center)
-    cone.SetResolution(10)
-
-    coneModelNode = slicer.vtkMRMLModelNode()
-    slicer.mrmlScene.AddNode(coneModelNode)
-    coneModelNode.SetName('ConeModel')
-    coneModelNodeToUpdate = coneModelNode
-    cone.Update()
-    coneModelNodeToUpdate.SetAndObservePolyData(cone.GetOutput())
-
-    if coneModelNodeToUpdate.GetDisplayNode() is None:
-      displayNode = slicer.vtkMRMLModelDisplayNode()
-      slicer.mrmlScene.AddNode(displayNode)
-      displayNode.SetName('ConeModelDisplay')
-      coneModelNodeToUpdate.SetAndObserveDisplayNodeID(displayNode.GetID())
+    resolution = 10
+    coneSource = testingLogic.createConeSource(coneDimensions, center, resolution)
+    coneModelNode = testingLogic.drawCone(coneSource, coneDimensions, center)
 
     #Find the tip of cone and the center of the base of the cone, points which
     #will be used as inputs for the landmark registration
-    coneModelPolyData = coneModelNode.GetPolyData()
-    numPointsInCone = coneModelPolyData.GetNumberOfPoints()
-    conePointsTupleArray = []
-    for point in range(numPointsInCone):
-      conePointsTupleArray.append(coneModelPolyData.GetPoint(point))
-    coneTip = conePointsTupleArray[0]
-    #Place fiducial at cone tip
-    markupsLogic = slicer.modules.markups.logic()
-    markupsLogic.SetDefaultMarkupsDisplayNodeGlyphScale(5.0)
-    markupsLogic.SetDefaultMarkupsDisplayNodeColor(0.0, 0.0, 0.0)
-    markupsLogic.SetDefaultMarkupsDisplayNodeSelectedColor(0.0, 0.0, 0.0)
-    markupsLogic.AddNewFiducialNode()
-    markupsLogic.AddFiducial(coneTip[0], coneTip[1], coneTip[2])
-    fidList = slicer.util.getNode('F')
-    fidList.SetNthFiducialLabel(0, 'Cone Tip')
+    coneTip = testingLogic.getConeTip(coneModelNode)
 
     #Compute center of mass of points of cone base
-    coneBasePoints = []
-    for index in range(1, numPointsInCone):
-      point = conePointsTupleArray[index]
-      coneBasePoints.append(point)
-    xSum, ySum, zSum = 0, 0, 0
-    for (x,y,z) in coneBasePoints:
-      xSum += x
-      ySum += y
-      zSum += z
-    coneBaseCenter_x = xSum / cone.GetResolution()
-    coneBaseCenter_y = ySum / cone.GetResolution()
-    coneBaseCenter_z = zSum / cone.GetResolution()
-    #Place fiducial at cone base
-    markupsLogic.AddFiducial(coneBaseCenter_x, coneBaseCenter_y, coneBaseCenter_z)
-    fidList.SetNthFiducialLabel(1, 'Cone Base Center')
+    coneBaseCenter = testingLogic.getCenterOfConeBase(coneSource, coneModelNode)
 
     #Create a third point for use in landmark registration, perpendicular to a
     #line drawn in the AP plane of the cone tip
-    pointCoplanarToConeTip = (coneTip[0], coneTip[1] + 100 , coneTip[2]) #Add arbitrary distance along plane
-    markupsLogic.AddFiducial(pointCoplanarToConeTip[0],pointCoplanarToConeTip[1],pointCoplanarToConeTip[2])
-    midpointOfConeTipLine = ((coneTip[0] + pointCoplanarToConeTip[0])/2 , (coneTip[1] + pointCoplanarToConeTip[1])/2, (coneTip[2] + pointCoplanarToConeTip[2])/2)
-    markupsLogic.AddFiducial(midpointOfConeTipLine[0],midpointOfConeTipLine[1],midpointOfConeTipLine[2])
-    perpendicularPoint = (midpointOfConeTipLine[0], midpointOfConeTipLine[1], midpointOfConeTipLine[2] + 100)
-    markupsLogic.AddFiducial(perpendicularPoint[0],perpendicularPoint[1],perpendicularPoint[2])
+    pointPairThree = testingLogic.computeThirdPointsForLandmarkRegistration(cameraPosition, coneTip)
 
+    #TODO: Compute landmark registration
 
-    self.delayDisplay('Test passed!')
+    #TODO: Check if collision is occcuring
+    self.delayDisplay('Non-collision test passed!')
